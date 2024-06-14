c472eca12d78124b60e301e80d10293a8b370d98
DimJimbo <dmtris2019@gmail.com>
2024-06-14 05:37:22 -0400

    Now the starting zoom level of the simulation can be changed

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import pymunk.pygame_util
 import time
 import random
 import sys
+import multiprocessing as mp
 
 pygame.init()
 pygame.key.set_repeat(200, 50) # so multiple KEYDOWN events get sent ( ie holding down a key works )
@@ -14,31 +15,33 @@ pygame.key.set_repeat(200, 50) # so multiple KEYDOWN events get sent ( ie holdin
 # ZOOM IN/OUT: Q/E
 # PAUSE SIMULATION: P
 
-
 class Constants: # Change stuff about the simulation here
 
-    # display/pygame stuff
-    fullscreen = False
-    size = (640, 800)
-
-    per_frame_draw = 1 # redraw screen every {per_frame_draw} frames
-    move_by = 20 # pixels
-    zoom_by = 0.1 # percent
-    min_zoom = zoom_by
-    max_zoom = 100*zoom_by
-
     # body related stuff
-    body_radius = 3
+    body_radius = 4
     body_mass = 1e6
     body_elasticity = 0.8
     body_friction = 0.6
 
+    # display/pygame stuff
+    fullscreen = False
+    width = 1200
+    height = 800
+
+    per_frame_draw = 1 # redraw screen every {per_frame_draw} frames
+    move_by = 20 # pixels
+    zoom_by = 0.05 # percent
+    min_zoom = 1/(2*body_radius) # point when round(body_radius*zoom_percent) is 0
+    max_zoom = 100*zoom_by
+    starting_zoom = 0.5
+
     # sim related stuff
-    bodies_N = 100 # starting body amount
+    bodies_N = 200 # starting body amount
+
     collision_iterations = 20 # Increasing this doesn't significantly help with errors
     gravity_min_dist_sqrd = (2*body_radius)**2 # min distance squared where gravity will be applied
-    gravity_max_dist_sqrd = 400**2 # max distance squared where gravity will be applied
-    per_frame_gravity = 3 # recalculate gravity every {per_frame_gravity} frames
+    gravity_max_dist_sqrd = 600**2 # max distance squared where gravity will be applied
+    per_frame_gravity = 5 # recalculate gravity every {per_frame_gravity} frames
     dt = 0.1
     G = 6e-5
 
@@ -88,25 +91,32 @@ class Simulator:
         if Constants.fullscreen:
             self._display = pygame.display.set_mode(flags=pygame.FULLSCREEN)
         else:
-            self._display = pygame.display.set_mode(Constants.size)
+            self._display = pygame.display.set_mode((Constants.width, Constants.height))
         self._window_size_vec = pymunk.Vec2d(self._display.get_width(), self._display.get_height())
 
         self.space = pymunk.Space()
         self.bodies = []
 
-        self.gravity_func = self._basic_gravity
-
-        self.init_sim()
+        self.gravity_func = self._basic_gravity # for if I ever implement Barnes-Hut or any other method
 
         self.is_paused = False # controls only the physics sim, not drawing
         self.position_displacement_vec = pymunk.Vec2d(0, 0) # change the origin of the simulation
-        self.zoom_percent = 1 # zoom in and out
+        self.zoom_percent = Constants.starting_zoom # zoom in and out
 
+        self.init_sim()
+
+    def get_display_position_from_pymunk_position(self, position):
+        # Thank stack overflow, everything except for that + self.position_diplacement_vec is for zooming
+        return (position - self._window_size_vec/2 + self.position_displacement_vec)*self.zoom_percent + self._window_size_vec/2
+    def get_pymunk_position_from_display_position(self, position):
+        # just the inverse of the one at the _update_display function
+        return (position - self._window_size_vec/2)/self.zoom_percent + self._window_size_vec/2 - self.position_displacement_vec
     def init_sim(self):
 
         # setup space
         self.space.gravity = (0, 0)
         self.space.iterations = Constants.collision_iterations
+
         handler = self.space.add_collision_handler(1, 1)
         handler.post_solve = custom_post_solve
         if Constants.use_spatial_hash:
@@ -115,12 +125,15 @@ class Simulator:
                 count=Constants.spatial_hash_count
             )
 
+
         # make all the bodies
         for _ in range(Constants.bodies_N):
-            position = (random.randint(0, self._display.get_width()), random.randint(0, self._display.get_height()))
+            # coordinates of body, based on screen dimensions
+            x = random.randint(0, Constants.width)
+            y = random.randint(0, Constants.height)
 
+            position = self.get_pymunk_position_from_display_position(pymunk.Vec2d(x, y)) # to work with different starting zoom levels
             self.add_body(position)
-
 
 
     def add_body(self, position):
@@ -139,11 +152,11 @@ class Simulator:
         # This gives the most performance out of all the things I tried
         # using numpy doesn't help at all from what I tested, it even makes the performances worse by a lot
 
-        forces = [pymunk.Vec2d.zero()] * len(bodies)
+        forces = [pymunk.Vec2d.zero()]*len(bodies)
 
-        for i, b1 in enumerate(bodies):
-            for j, b2 in enumerate(bodies[i + 1:]):
-                diff_vec = b2.position - b1.position
+        for i, body1 in enumerate(bodies):
+            for j, body2 in enumerate(bodies[i + 1:]):
+                diff_vec = body2.position - body1.position
                 dist_sqrd = diff_vec.dot(diff_vec)  # faster than b1.position.get_dist_sqrd(b2.position)
 
                 if (dist_sqrd < Constants.gravity_min_dist_sqrd or
@@ -151,15 +164,18 @@ class Simulator:
                     continue
 
                 F_dir = diff_vec.normalized()
-                F_mag = Constants.G * b1.mass * b2.mass / dist_sqrd
-                F = F_dir * F_mag
+                F_mag = Constants.G*body1.mass*body2.mass/dist_sqrd
+                F = F_dir*F_mag
 
                 forces[i] += F
                 forces[i + 1 + j] -= F
 
             # this instead of at local point helps a bit with errors
-            b1.apply_force_at_world_point(forces[i], b1.position)
+            body1.apply_force_at_world_point(forces[i], body1.position)
 
+        # for i, body1 in enumerate(self.bodies):
+        #     # this instead of at local point helps a bit with errors
+        #     body1.apply_force_at_world_point(forces[i], body1.position)
     def _update_display(self):
         # This helps a lot with performance, mainly bc its drawing simple circles instead of force arrows too
         # Also, only way to add zooming and panning to the sim
@@ -167,17 +183,18 @@ class Simulator:
         self._display.fill((0, 0, 0))
 
         for body in self.bodies:
-            # Thank stack overflow, everything except for that + self.position_diplacement_vec is for zooming
-            position = ((body.position - self._window_size_vec/2 + self.position_displacement_vec)*self.zoom_percent + self._window_size_vec/2)
+
+            position = self.get_display_position_from_pymunk_position(body.position)
 
             # don't draw things outside the screen
             if (position.x > self._window_size_vec.x or position.x < 0 or
                 position.y > self._window_size_vec.y or position.y < 0):
                 continue
-            radius = (Constants.body_radius*self.zoom_percent)
+
+            radius = round(Constants.body_radius*self.zoom_percent)
             pygame.draw.circle(self._display, (255, 255, 255), position, radius)
 
-        pygame.display.flip()
+        pygame.display.update()
 
     def start(self):
 
@@ -206,8 +223,7 @@ class Simulator:
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     # 1 is left click, 3 is right click
                     if event.button == 1:
-                        # just the inverse of the one at the _update_display function
-                        position = (pygame.mouse.get_pos() - self._window_size_vec/2)/self.zoom_percent + self._window_size_vec/2 - self.position_displacement_vec
+                        position = self.get_pymunk_position_from_display_position(pygame.mouse.get_pos())
                         self.add_body(position)
 
                 elif event.type == pygame.KEYDOWN:
@@ -223,9 +239,9 @@ class Simulator:
                     elif event.key == pygame.K_d:
                         self.position_displacement_vec += (-Constants.move_by, 0)
 
-                    elif event.key == pygame.K_q and self.zoom_percent > Constants.min_zoom:
+                    elif event.key == pygame.K_q and self.zoom_percent - Constants.zoom_by > Constants.min_zoom:
                         self.zoom_percent -= Constants.zoom_by
-                    elif event.key == pygame.K_e and self.zoom_percent < Constants.max_zoom:
+                    elif event.key == pygame.K_e and self.zoom_percent + Constants.zoom_by < Constants.max_zoom:
                         self.zoom_percent += Constants.zoom_by
 
             if not self.is_paused:
